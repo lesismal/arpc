e858323a4cf9548dbf12204d75ee7bfaa5d92a23
lesismal <wuqiongjishu@163.com>
2020-05-22 20:29:13 +0800

    + examples/kcp

---
+++ examples/kcp/client/client.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"crypto/sha1"
+	"log"
+	"net"
+	"time"
+
+	"github.com/lesismal/arpc"
+	"github.com/xtaci/kcp-go"
+	"golang.org/x/crypto/pbkdf2"
+)
+
+func main() {
+	client, err := arpc.NewClient(func() (net.Conn, error) {
+		key := pbkdf2.Key([]byte("demo pass"), []byte("demo salt"), 1024, 32, sha1.New)
+		block, _ := kcp.NewAESBlockCrypt(key)
+		return kcp.DialWithOptions("localhost:8888", block, 10, 3)
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	client.Run()
+	defer client.Stop()
+
+	req := "hello"
+	rsp := ""
+	err = client.Call("/echo", &req, &rsp, time.Second*5)
+	if err != nil {
+		log.Fatalf("Call failed: %v", err)
+	} else {
+		log.Printf("Call Response: \"%v\"", rsp)
+	}
+}
---
+++ examples/kcp/server/server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"crypto/sha1"
+	"log"
+
+	"github.com/lesismal/arpc"
+	"github.com/xtaci/kcp-go"
+	"golang.org/x/crypto/pbkdf2"
+)
+
+func main() {
+	key := pbkdf2.Key([]byte("demo pass"), []byte("demo salt"), 1024, 32, sha1.New)
+	block, _ := kcp.NewAESBlockCrypt(key)
+	ln, err := kcp.ListenWithOptions(":8888", block, 10, 3)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	svr := arpc.NewServer()
+
+	// register router
+	svr.Handler.Handle("/echo", func(ctx *arpc.Context) {
+		str := ""
+		ctx.Bind(&str)
+		ctx.Write(str)
+	})
+
+	svr.Serve(ln)
+}
