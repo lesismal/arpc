e15a388b461703c0fa378e30ed9bbe665f2e05b7
lesismal <wuqiongjishu@163.com>
2020-09-22 02:01:02 +0800

    - for build

--- client_test.go
+++ client_test.go
@@ -282,11 +282,11 @@ func newSvr() *Server {
 }
 
 func TestWebsocket(t *testing.T) {
-	ln, _ := websocket.NewListener(":16790", nil)
+	ln, _ := websocket.NewListener(":13456", nil)
 	defer ln.Close()
 	http.HandleFunc("/ws", ln.(*websocket.Listener).Handler)
 	go func() {
-		err := http.ListenAndServe(":16790", nil)
+		err := http.ListenAndServe(":13456", nil)
 		if err != nil {
 			t.Fatal("ListenAndServe: ", err)
 		}
@@ -303,7 +303,7 @@ func TestWebsocket(t *testing.T) {
 	go svr.Serve(ln)
 
 	client, err := NewClient(func() (net.Conn, error) {
-		return websocket.Dial("ws://localhost:16790/ws")
+		return websocket.Dial("ws://localhost:13456/ws")
 	})
 	if err != nil {
 		panic(err)
