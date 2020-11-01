ae1f295b43273c172a972c30cf509fbc9043b289
lesismal <wuqiongjishu@163.com>
2020-11-01 13:30:50 +0800

    + graceful example

---
+++ examples/graceful/client/client.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"net"
+	"sync"
+	"time"
+
+	"github.com/json-iterator/go"
+	"github.com/lesismal/arpc"
+)
+
+func main() {
+	client, err := arpc.NewClient(func() (net.Conn, error) {
+		return net.DialTimeout("tcp", "localhost:8888", time.Second*3)
+	})
+	if err != nil {
+		panic(err)
+	}
+	client.Codec = jsoniter.ConfigCompatibleWithStandardLibrary
+	defer client.Stop()
+
+	wg := sync.WaitGroup{}
+	for i := 0; i < 9; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			req := "hello"
+			rsp := ""
+			err = client.Call("/echo", &req, &rsp, time.Second*10)
+			if err != nil {
+				log.Fatalf("Call /echo failed: %v", err)
+			} else {
+				log.Printf("Call /echo Response: \"%v\"", rsp)
+			}
+		}()
+	}
+	wg.Wait()
+
+	req := "hello"
+	rsp := ""
+	err = client.Call("/echo", &req, &rsp, time.Second*10)
+	if err != nil {
+		log.Fatalf("Call /echo failed: %v", err)
+	} else {
+		log.Printf("Call /echo Response: \"%v\"", rsp)
+	}
+}
