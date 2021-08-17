681faa83234247c8661d98288e3691d354a85844
lesismal <wuqiongjishu@163.com>
2021-08-17 11:01:27 +0800

    update nbio with pool example

--- examples/nbio/server/server.go
+++ examples/nbio/server/server.go
@@ -4,7 +4,10 @@ import (
 	"bytes"
 	"os"
 	"os/signal"
+	"runtime"
+	"sync"
 	"syscall"
+	"unsafe"
 
 	"github.com/lesismal/arpc"
 	"github.com/lesismal/arpc/codec"
@@ -21,6 +24,7 @@ var (
 
 // Session .
 type Session struct {
+	bufMux sync.Mutex
 	*arpc.Client
 	bytes.Buffer
 }
@@ -39,22 +43,35 @@ func onData(c *nbio.Conn, data []byte) {
 		c.Close()
 		return
 	}
+
 	session := iSession.(*Session)
-	session.Write(data)
-	for session.Len() >= arpc.HeadLen {
-		headBuf := session.Bytes()[:4]
-		header := arpc.Header(headBuf)
-		total := arpc.HeadLen + header.BodyLen()
-		if session.Len() < total {
-			return
+	session.bufMux.Lock()
+	session.Buffer.Write(data)
+	session.bufMux.Unlock()
+
+	c.Execute(func() {
+		for {
+			session.bufMux.Lock()
+			if session.Len() < arpc.HeadLen {
+				session.bufMux.Unlock()
+				return
+			}
+			headBuf := session.Bytes()[:4]
+			header := arpc.Header(headBuf)
+			total := arpc.HeadLen + header.BodyLen()
+			if session.Len() < total {
+				session.bufMux.Unlock()
+				return
+			}
+
+			buffer := mempool.Malloc(total)
+			session.Buffer.Read(buffer)
+			session.bufMux.Unlock()
+
+			msg := handler.NewMessageWithBuffer(buffer)
+			handler.OnMessage(session.Client, msg)
 		}
-
-		buffer := mempool.Malloc(total)
-		session.Read(buffer)
-
-		msg := handler.NewMessageWithBuffer(buffer)
-		handler.OnMessage(session.Client, msg)
-	}
+	})
 }
 
 func main() {
@@ -64,6 +81,7 @@ func main() {
 
 	handler.EnablePool(true)
 	handler.SetAsyncWrite(false)
+	handler.SetAsyncResponse(true)
 
 	// register router
 	handler.Handle("/echo", func(ctx *arpc.Context) {
@@ -88,6 +106,19 @@ func main() {
 	g.OnWriteBufferRelease(func(c *nbio.Conn, b []byte) {
 		mempool.Free(b)
 	})
+	g.Execute = func(f func()) {
+		go func() {
+			defer func() {
+				if err := recover(); err != nil {
+					const size = 64 << 10
+					buf := make([]byte, size)
+					buf = buf[:runtime.Stack(buf, false)]
+					nlog.Error("Execute failed: %v\n%v\n", err, *(*string)(unsafe.Pointer(&buf)))
+				}
+			}()
+			f()
+		}()
+	}
 
 	err := g.Start()
 	if err != nil {
