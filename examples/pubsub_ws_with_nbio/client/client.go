feffba68a62f95ff7dc8b179adef18e6db2c347c
lesismal <lesismal@gmail.com>
2022-04-03 20:46:49 +0800

    pubsub: add example with nbio/websocket

---
+++ examples/pubsub_ws_with_nbio/client/client.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/lesismal/arpc/extension/protocol/websocket"
+	"github.com/lesismal/arpc/extension/pubsub"
+	"github.com/lesismal/arpc/log"
+)
+
+var (
+	password = "123qwe"
+
+	topicName = "Broadcast"
+)
+
+func onTopic(topic *pubsub.Topic) {
+	log.Info("[OnTopic] [%v] \"%v\", [%v]",
+		topic.Name,
+		string(topic.Data),
+		time.Unix(topic.Timestamp/1000000000, topic.Timestamp%1000000000).Format("2006-01-02 15:04:05.000"))
+}
+
+func consumer(c *pubsub.Client) {
+	if err := c.Subscribe(topicName, onTopic, time.Second); err != nil {
+		panic(err)
+	}
+}
+
+func producer(c *pubsub.Client) {
+	ticker := time.NewTicker(time.Second)
+	for i := 0; true; i++ {
+		_, ok := <-ticker.C
+		if !ok {
+			break
+		}
+		if i%5 == 0 {
+			c.Publish(topicName, fmt.Sprintf("message from client %d", i), time.Second)
+		} else {
+			c.PublishToOne(topicName, fmt.Sprintf("message from client %d", i), time.Second)
+		}
+	}
+}
+
+func dialer() (net.Conn, error) {
+	return websocket.Dial("ws://localhost:8888/ws")
+}
+
+func newClient() *pubsub.Client {
+	client, err := pubsub.NewClient(dialer)
+	if err != nil {
+		panic(err)
+	}
+	// client.Password = password
+
+	// authentication
+	err = client.Authenticate()
+	if err != nil {
+		panic(err)
+	}
+
+	return client
+}
+
+func main() {
+	{
+		for i := 0; i < 5; i++ {
+			client := newClient()
+			defer client.Stop()
+			consumer(client)
+		}
+	}
+
+	{
+		client := newClient()
+		defer client.Stop()
+		go producer(client)
+	}
+
+	<-make(chan int)
+}
---
+++ examples/pubsub_ws_with_nbio/server/server.go
@@ -0,0 +1,205 @@
+/*
+ * Copyright 2021 CloudWeGo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"runtime"
+	"time"
+
+	"github.com/lesismal/arpc"
+	"github.com/lesismal/arpc/codec"
+	"github.com/lesismal/arpc/extension/pubsub"
+	"github.com/lesismal/nbio/mempool"
+	"github.com/lesismal/nbio/nbhttp"
+	"github.com/lesismal/nbio/nbhttp/websocket"
+	"github.com/lesismal/nbio/taskpool"
+)
+
+var (
+	psServer *pubsub.Server
+
+	executePool = taskpool.NewMixedPool(1024*4, 1, 1024*runtime.NumCPU())
+)
+
+func onWebsocket(w http.ResponseWriter, r *http.Request) {
+	// if err := authenticate(...); err != nil {
+	// 	some log
+	// 	return
+	// }
+
+	upgrader := newUpgrader()
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		conn.Close()
+		return
+	}
+	wsConn := conn.(*websocket.Conn)
+	wsConn.SetReadDeadline(time.Now().Add(120 * time.Second))
+}
+
+func main() {
+	// disable frameworks' log
+	{
+		// alog.SetLevel(alog.LevelNone)
+		// nlog.SetLevel(nlog.LevelNone)
+	}
+
+	// init pubsub server handler for nbhttp
+	{
+		// psServer.Handler.EnablePool(true)
+		arpc.SetAsyncResponse(true)
+		psServer = pubsub.NewServer()
+		// must set async write for nbio
+		psServer.Handler.SetAsyncWrite(false)
+	}
+
+	mux := &http.ServeMux{}
+	mux.HandleFunc("/ws", onWebsocket)
+
+	svr := nbhttp.NewEngine(nbhttp.Config{
+		Network:                 "tcp",
+		Addrs:                   []string{"localhost:8888"},
+		Handler:                 mux,
+		ReleaseWebsocketPayload: true,
+	})
+
+	err := svr.Start()
+	if err != nil {
+		fmt.Printf("nbio.Start failed: %v\n", err)
+		return
+	}
+
+	go func() {
+		topicName := "Broadcast"
+		for i := 0; true; i++ {
+			time.Sleep(time.Second)
+			psServer.Publish(topicName, fmt.Sprintf("message from server %v", i))
+		}
+	}()
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+	<-interrupt
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	svr.Shutdown(ctx)
+}
+
+type WebsocketConn struct {
+	*websocket.Conn
+}
+
+func (c *WebsocketConn) Write(data []byte) (int, error) {
+	err := c.Conn.WriteMessage(websocket.BinaryMessage, data)
+	if err != nil {
+		return 0, err
+	}
+	return len(data), nil
+}
+
+// Session .
+type Session struct {
+	*arpc.Client
+	cache []byte
+}
+
+func newUpgrader() *websocket.Upgrader {
+	u := websocket.NewUpgrader()
+	u.OnOpen(onOpen)
+	u.OnMessage(onMessage)
+	u.OnClose(onClose)
+	return u
+}
+
+func onOpen(c *websocket.Conn) {
+	client := &arpc.Client{Conn: &WebsocketConn{c}, Codec: codec.DefaultCodec, Handler: psServer.Handler}
+	client.SetState(true)
+	session := &Session{
+		Client: client,
+	}
+	c.SetSession(session)
+}
+
+func onClose(c *websocket.Conn, err error) {
+	iSession := c.Session()
+	if iSession == nil {
+		c.Close()
+		return
+	}
+	session, ok := iSession.(*Session)
+	if ok {
+		if session.cache != nil {
+			mempool.Free(session.cache)
+			session.cache = nil
+		}
+		psServer.Handler.OnDisconnected(session.Client)
+	}
+}
+
+func onMessage(c *websocket.Conn, mt websocket.MessageType, data []byte) {
+	iSession := c.Session()
+	if iSession == nil {
+		c.Close()
+		return
+	}
+
+	session := iSession.(*Session)
+	start := 0
+	if session.cache != nil {
+		session.cache = append(session.cache, data...)
+		data = session.cache
+	}
+	for {
+		if len(data)-start < arpc.HeadLen {
+			goto Exit
+		}
+		header := arpc.Header(data[start : start+4])
+		total := arpc.HeadLen + header.BodyLen()
+		if len(data)-start < total {
+			goto Exit
+		}
+
+		buffer := mempool.Malloc(total)
+		copy(buffer, data[start:start+total])
+		start += total
+		msg := psServer.Handler.NewMessageWithBuffer(buffer)
+		psServer.Handler.OnMessage(session.Client, msg)
+	}
+
+Exit:
+	if session.cache != nil {
+		if start == len(data) {
+			mempool.Free(data)
+			session.cache = nil
+		} else {
+			left := mempool.Malloc(len(data) - start)
+			copy(left, data[start:])
+			mempool.Free(data)
+			session.cache = left
+		}
+	} else if start < len(data) {
+		left := mempool.Malloc(len(data) - start)
+		copy(left, data[start:])
+		mempool.Free(data)
+		session.cache = left
+	}
+}
